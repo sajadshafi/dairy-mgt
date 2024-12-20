2c6afe7a4b249ad1110a2d82469f6a32177f64ac
Sajad Shafi <34324515+sajadshafi@users.noreply.github.com>
2024-12-20 18:03:00 +0530

    Update EmailConstants.cs
    removed critical data

--- DairyManagementSystem/EmailConfig/EmailConstants.cs
+++ DairyManagementSystem/EmailConfig/EmailConstants.cs
@@ -2,8 +2,8 @@
    public class EmailConstants {
       public const string HOST = "smtp.gmail.com";
       public const int PORT = 587;
-      public const string USERNAME = "wasimahmadbca@gmail.com";
-      public const string PASSWORD = "gqkxkhqelzvkvmwp";
+      public const string USERNAME = "sdfga@yopmail.com";
+      public const string PASSWORD = "abc";
       public const string SECURITY = "STARTTLS";
    }
 }
